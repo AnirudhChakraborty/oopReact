4df5d74d8a0de145e9a9087f67eecb2e69f6f61b
ghazikhan <ghazi.khan@theleanapps.com>
2018-09-12 12:50:22 +0530

    Table Branch

---
+++ src/components/GridTable/GridTable.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import {Table} from 'reactstrap';
+
+export default class GridTable extends Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <Table>
+        <thead>
+
+        </thead>
+      </Table>
+    );
+  }
+}
