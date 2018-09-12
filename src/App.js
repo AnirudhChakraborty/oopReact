774db786d09ef010cec06d9a391f41c8138aeb49
Anirudha Chakraborty <ranvijay.singh@theleanapps.com>
2018-09-12 16:41:46 +0530

    Added modal component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ModalComponent from './components/ModalComponent'
+import { Button} from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {  
+      isVisible : false
+    }
+
+  }
+
   render() {
+    let {isVisible} = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className="App-logo" alt="logo" onClick={this.showModal}/>
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
---
+++ src/components/ModalComponent/ModalComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import {  Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+class ModalComponet extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            isVisible : false
+        }
+    }
+
+    getDerivedStateFromProps(nextProps, prevState) {
+        //Check if modal is kept visible
+        let {isVisible} = nextProps
+        if(isVisible !== prevState.isVisible){
+            return { isVisible : isVisible};
+        }
+        else return null;
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        let {isVisible} = prevProps
+        if(isVisible !== this.props.isVisible){
+          this.setState({isVisible: isVisible});
+        }
+    }      
+
+    toggle = () => {
+        let {isVisible} = this.state
+        this.setState({isVisible : !isVisible})
+    }
+
+    render() {
+         /*** 
+         *  @param : isVisible -> Boolean
+         *  @param : modalHeader -> Const;
+         *  @param : modalBody -> Const
+         *  @param : modalFooter -> Const 
+         *  @param : showBackdrop -> Boolean 
+         *  @param : screenSize -> lg/sm (large screen/small screen)
+         *  @param : onClosed -> function()
+         *  @param : canBeCancelled -> Boolean
+         * **/
+        
+        let {isVisible} = this.state
+        let {modalHeader, modalBody, modalFooter, 
+            showBackdrop, screenSize, canBeCancelled, onClosed} = this.props
+    
+        return (
+            <Modal isOpen={isVisible} backdrop={showBackdrop}
+                     size={screenSize} onClosed={onClosed} toggle={canBeCancelled ? this.toggle : null}>
+            <ModalHeader toggle={canBeCancelled ? this.toggle : null}> {modalHeader} </ModalHeader>
+            <ModalBody>   {modalBody} </ModalBody>
+            <ModalFooter> {modalFooter} </ModalFooter>
+            </Modal>
+        );
+    }
+}
+
+export default ModalComponet;
