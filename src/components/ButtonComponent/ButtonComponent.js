c979827ad2555de55fbc4c75a564b9617958ffdd
ghazikhan <ghazi.khan@theleanapps.com>
2018-09-12 16:24:12 +0530

    added comments

--- src/components/ButtonComponent/ButtonComponent.js
+++ src/components/ButtonComponent/ButtonComponent.js
@@ -3,6 +3,15 @@ import PropTypes from 'prop-types';
 import {Button} from "reactstrap";
 
 export default class ButtonComponent extends Component{
+
+  /**
+   * Button Component takes 2 props from parent component
+   * 1. onClick: it will have the function
+   * 2. config: it will have object of configuration to alter button look and feel
+   *
+   * ** children is the props which is passed inside the open/close tag of component will be treated as children
+   */
+
   render(){
     return(
       <Button onClick={this.props.onClick} {...this.props.config}>{this.props.children}</Button>
